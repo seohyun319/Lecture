ea1013a74193032b03389779fa4b5d250f788e33
seohyun319 <tjguselly0319@gmail.com>
2021-07-29 16:05:43 +0900

    MovieDetail Page 만들기 - 2. api 정보를 state에 집언허기 & 전체적인 template 간단히 만들기

--- node_react_movie_app/client/src/components/views/MovieDetail/MovieDetail.js
+++ node_react_movie_app/client/src/components/views/MovieDetail/MovieDetail.js
@@ -1,18 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { API_URL, API_KEY } from '../../Config'
+import MainImage from '../../views/LandingPage/Sections/MainImage';
+import MovieInfo from './Sections/MovieInfo';
 
 function MovieDetailPage(props) {
 
     let movieId = props.match.params.movieId 
+    const [Movie, setMovie] = useState([])
 
     useEffect(() => { //useEffect는 dom이 로드되면 할 동작을 넣어주는 것
 
         let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
-				let endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
+		let endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
         fetch(endpointInfo)  //fetch를 이용해 정보 가져옴
             .then(response => response.json()) //정보 json으로 받아옴
             .then(response => {
                 console.log(response)
+                setMovie(response)
             })
 
     }, [])
@@ -20,9 +24,34 @@ function MovieDetailPage(props) {
 
 
 		return (
-		        <div>
-		            
-		        </div>
+            <div>
+                {/* Header */}
+                
+                    <MainImage
+                        image={`${IMAGE_BASE_URL}${IMAGE_SIZE}${Movie.backdrop_path}`}
+                        title={Movie.original_title}     /* 현재 state이 movie니까 movie로 넣어줌*/
+                        text={Movie.overview}
+                    />              
+
+                {/* Body */}
+                <div style={{ width: '85%', margin: '1rem auto' }}>
+
+                    {/* Movie Info */}
+                    
+                        <MovieInfo 
+                            movie={Movie} /> //movie state에 movie 넣어줌
+                                           
+
+                    <br />
+                    {/* Actors Grid*/}
+
+                    <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
+                        <Button>Toggle Actor View </Button>
+                    </div>
+
+                </div>
+
+            </div>
 		    )
 		}
 		
