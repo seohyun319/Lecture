b5a6d7586c2fc324a39e9d6e98379a3013f7d7e4
seohyun319 <tjguselly0319@gmail.com>
2021-07-29 15:09:13 +0900

    LandingPage 만들기 - 3 MainImage Component 만들기

--- node_react_movie_app/client/src/components/views/LandingPage/LandingPage.js
+++ node_react_movie_app/client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import { API_URL, API_KEY } from '../../Config';
+import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
+import axios from 'axios';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 function LandingPage() {
 
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
 
-    const [Movies, setMovies] = useState([])  //array로 usestate 정보 받아옴
-
-    useEffect(() => {   //api 가져옴
+    useEffect(() => {
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-        fetchMovies(endpoint)  //현재 인기있는 영화 가져옴
+        fetchMovies(endpoint)
 
     }, [])
 
@@ -20,18 +25,21 @@ function LandingPage() {
                 console.log(response)
                 setMovies([...response.results])
                 setMainMovieImage(response.results[0])
-                setCurrentPage(response.page)
             })
     }
-
-  
 
 
     return (
         <div style={{ width: '100%', margin: '0' }}>
 
             {/* Main Image */}
-            
+            {MainMovieImage &&
+                <MainImage
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+            }
 
 
             <div style={{ width: '85%', margin: '1rem auto' }}>
@@ -41,10 +49,11 @@ function LandingPage() {
 
                 {/* Movie Grid Cards */}
 
+
             </div>
 
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <button> Load More</button>
+                <button onClick={loadMoreItems}> Load More</button>
             </div>
 
         </div>
