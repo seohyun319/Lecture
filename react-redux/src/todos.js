505a3c5c9d2232e7bab6949f12c9729be555c711
seohyun319 <tjguselly0319@gmail.com>
2022-07-07 23:27:01 +0900

    #2.1 State Mutation

--- react-redux/src/todos.js
+++ react-redux/src/todos.js
@@ -6,11 +6,17 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+// store를 수정할 수 있는 유일한 방법은 action을 보내는 것.
+// state를 mutate하면 안 되고 new state objects를 return해야 함.
+// mutate: 기존 상태 수정
+// 반드시 새 state를 만들어야서 return해야함. 기존 거 건드리면 안 됨.
 const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      // 이전 state와 새 TODO를 가짐
+      // 이전 array의 object들과 새 object를 합쳐 새 array를 만듦
+      return [...state, { text: action.text, id: Date.now() }];
     case DELETE_TODO:
       return [];
     default:
@@ -19,6 +25,8 @@ const reducer = (state = [], action) => {
 };
 
 const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
 
 const onSubmit = (e) => {
   e.preventDefault();
