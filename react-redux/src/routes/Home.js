355e29fc09bed42f290b7dd7e0c92b5cb7d817ac
seohyun319 <tjguselly0319@gmail.com>
2022-07-08 03:21:17 +0900

    #3.3 mapDispatchToProps

--- react-redux/src/routes/Home.js
+++ react-redux/src/routes/Home.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function Home({ toDos }) {
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
+    addToDo(text);
     setText("");
   }
   return (
@@ -27,8 +29,18 @@ function mapStateToProps(state) {
   return { toDos: state };
 }
 
+// mapDispatchToProps(dispatch, ownProps?)
+// Home에서 props 바꿀 수 있음.
+function mapDispatchToProps(dispatch) {
+  return {
+    // addToDo는 dispatch 호출
+    // dispatch는 actionCreators 호출
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+
 // connect: component를 store에 연결해줌.
 // connect(state)(dispatch)
 // mapStateToProps로 store에서 받아온 state를 component Home과 함께 가져옴
 // redux state로부터 home component에 prop으로 전달
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
