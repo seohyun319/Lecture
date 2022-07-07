f15fb603a21325c252c8616e8212a4f92aac4b1a
seohyun319 <tjguselly0319@gmail.com>
2022-07-07 22:37:16 +0900

    #1.4 Recap Refactor

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,23 +1,33 @@
 import { createStore } from "redux";
 
+// store 생성 ->
+// store 인자인 reducer는 state 수정 ->
+// reducer는 인자로 현재 state와 action을 받고 변경된 state return
+// dispatch가 reducer 불러 current state와 action을 더함
+// 변경 사항을 store에서 감지하고 싶으면 subscribe 하면 됨.
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 number.innerText = 0;
 
+// 실수 방지를 위해 변수로 선언
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 // reducer는 data(state)를 수정하는 유일한 함수
 // reducer(Modifier)가 return하는 건 application의 data가 됨
 // 인자로 state를 받고, 수정된 state return
 // count=0 : count의 defaultState는 0
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -37,12 +47,12 @@ countStore.subscribe(onChange);
 // dispatch: reducer에게 특정 동작 하라고 action 보냄.
 // dispatch 인자에 action 넘김.
 const handleAdd = () => {
-  // action은 {type: "ADD"}
-  countStore.dispatch({ type: "ADD" });
+  // action은 {type: "ADD"}. object여야 함, string 불가. 무조건 type이 있어야 함.
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
