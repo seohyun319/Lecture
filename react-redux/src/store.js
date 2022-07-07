56e6029024c080b880f134225ab7522eb776a553
seohyun319 <tjguselly0319@gmail.com>
2022-07-08 05:27:28 +0900

    #4.3 configureStore

--- react-redux/src/store.js
+++ react-redux/src/store.js
@@ -1,21 +1,7 @@
-import { createStore } from "redux";
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
 
 export const addToDo = createAction("ADD");
 export const deleteToDo = createAction("DELETE");
-
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     // addToDo는 함수이고, addToDo.type은 text인 "ADD"임
-//     case addToDo.type:
-//       // action에서 넘어온 거는 payload로 받음
-//       return [{ text: action.payload, id: Date.now() }, ...state];
-//     case deleteToDo.type:
-//       return state.filter((toDo) => toDo.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// };
 
 // createReducer() 사용하면 state를 mutate하기 쉽게 만듦. switch와 case 사용 안 해도 됨.
 // 첫 번째 arg는 initialState
@@ -30,6 +16,8 @@ const reducer = createReducer([], {
     state.filter((toDo) => toDo.id !== action.payload),
 });
 
-const store = createStore(reducer);
+// configureStore()을 사용하면 redux developer tools 사용 가능
+// 오...대박임... 크롬 개발자도구로 보기! 엣지에선 안 보였음
+const store = configureStore({ reducer });
 
 export default store;
