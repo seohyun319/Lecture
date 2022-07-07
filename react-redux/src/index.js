75e4369d7a2183be98a70cca718f0b88875274f9
seohyun319 <tjguselly0319@gmail.com>
2022-07-07 22:33:44 +0900

    #1.3 Subscriptions

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -4,6 +4,8 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
 // reducer는 data(state)를 수정하는 유일한 함수
 // reducer(Modifier)가 return하는 건 application의 data가 됨
 // 인자로 state를 받고, 수정된 state return
@@ -25,13 +27,23 @@ const countModifier = (count = 0, action) => {
 // createStore(reducer);
 const countStore = createStore(countModifier);
 
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+// subscribe: store 안의 변화를 알 수 있게 해줌
+countStore.subscribe(onChange);
+
 // dispatch: reducer에게 특정 동작 하라고 action 보냄.
 // dispatch 인자에 action 넘김.
-countStore.dispatch({ type: "ADD" }); // action은 {type: "ADD"}
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const handleAdd = () => {
+  // action은 {type: "ADD"}
+  countStore.dispatch({ type: "ADD" });
+};
 
-console.log(countStore.getState());
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
